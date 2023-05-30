0678e4730f4eb78e06fe052ccb9df448d25c697a
Kayque Castro <109294806+Kayllas@users.noreply.github.com>
2023-05-29 23:24:48 -0300

    Add files via upload
    First up

---
+++ restapi/src/main/java/com/interview/restapi/entidade/Contato.java
@@ -0,0 +1,59 @@
+package com.interview.restapi.entidade;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity
+public class Contato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    @NotBlank
+    private String nome;
+    @Column(nullable = false)
+    private String telefone;
+    @Column(nullable = false)
+    @Email
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
